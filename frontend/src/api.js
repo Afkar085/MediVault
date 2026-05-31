ddcb9886e98dda80cf2d9badb84c19115d1a11df
afkar <afkarkolkar@gmail.com>
2026-05-31 17:29:35 +0530

    Day 11: update frontend API URL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: 'https://profound-smile-production-f645.up.railway.app/api/v1',
 });
 
 API.interceptors.request.use((config) => {
